4d26488db1f00d70cd268927e29adf037592e1c8
rambhardwajj <rambhardwaj101000@gmail.com>
2023-06-26 01:13:29 +0530

    todoServer done

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -39,11 +39,85 @@
 
   Testing the server - run `npm run test-todoServer` command in terminal
  */
+
+  
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const port = 8000;
 const app = express();
 
+var todoList = [];
+
+let it = 0;
+todoList[0] = {
+  id: it + 1,
+  title: "Buy groceries",
+  completed: false,
+  description: "I should buy groceries"
+}; // Adding an initial item to the todoList array
+
 app.use(bodyParser.json());
+
+
+function printTodos(req, res) {
+  const ans = JSON.stringify(todoList);
+  console.log(ans);
+  res.send(ans);
+}
+app.get('/todos', printTodos);
+
+function getTodoItem(req, res) {
+  var idd = req.params.id;
+  if (todoList[idd]) {
+    const ans = todoList[idd].title;
+    console.log(ans);
+    res.send(ans);
+  } else {
+    res.sendStatus(404);
+  }
+}
+app.get('/todos/:id', getTodoItem);
+
+function addTodoItem(req, res) {
+  const newTodo = req.body;
+  const newTodoId = todoList.length + 1;
+  newTodo.id = newTodoId;
+  todoList.push(newTodo);
+  console.log(JSON.stringify(newTodo));
+  res.status(201).json({ id: newTodoId });
+}
+app.post('/todos', addTodoItem);
+
+function updateTodo( req, res){
+  const idd = req.params.id;
+  const { title , completed } = req.body;
+  const todoIndex = todoList.findIndex( item => item.id === parseInt(idd));
+  if( todoIndex!=-1){
+    todoList[todoIndex].title = title || todoList[todoIndex].title;
+    todoList[todoIndex].completed = completed || todoList[todoIndex].completed;
+    res.send(202);
+  }else{
+    res.sendStatus(404);
+  }
+}
+app.put('/todos/:id', updateTodo);
+
+function deleteTodo(req, res) {
+  const idd = req.params.id;
+  if (idd >= 1 && idd <=todoList.length) {
+    todoList.splice(idd-1, 1);
+    res.sendStatus(202);
+  } else {
+    res.sendStatus(404);
+  }
+}
+app.delete('/todos/:id', deleteTodo);
+
+app.listen(port, started);
+
+function started() {
+  console.log(`Example app running on port ${port}`);
+}
+
 
 module.exports = app;
